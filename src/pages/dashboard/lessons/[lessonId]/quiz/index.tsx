43bb623dbfec3484c359eb36631ee4f1ef867ba6
stephenwayar <stephenbuluswayar@gmail.com>
2023-06-18 23:54:38 +0100

    fix: remove css max-w inconsistency

--- src/pages/dashboard/lessons/[lessonId]/quiz/index.tsx
+++ src/pages/dashboard/lessons/[lessonId]/quiz/index.tsx
@@ -11,7 +11,7 @@ import preview_subject from '../../../../../assets/svgs/empty_state.svg'
 import QuizCard from "@/components/assessments/QuizCard";
 import ProfileNav from "@/components/nav/ProfileNav";
 
-export default function Quiz (){
+export default function Quiz() {
   const router = useRouter();
   const [opened, { open, close }] = useDisclosure(false);
   const [bulkAnswers, setBulkAnswers] = useState<any>([])
@@ -51,7 +51,7 @@ export default function Quiz (){
     }
   ]
 
-  const currentQuestion = question[currentQuestionIndex]; 
+  const currentQuestion = question[currentQuestionIndex];
 
   const handleNext = () => {
     if (currentQuestionIndex < question.length - 1) {
@@ -83,7 +83,7 @@ export default function Quiz (){
       questionId: currentQuestion.id,
       answer
     };
-    bulkAnswers.push(answerObj); 
+    bulkAnswers.push(answerObj);
     // handle submit mutation
     open()
   }
@@ -107,153 +107,43 @@ export default function Quiz (){
       <ProfileNav />
 
       <Box className="w-full px-4 sm:px-6 lg:px-8 mt-4 hidden lg:block">
-        <Box className='w-fit'>
-          <Link href={`/dashboard/subjects/${router.query.lessonId}`}>
-            <Flex className="max-w-[97rem] mx-auto space-x-2">
-              <Center className="bg-[#FEEDD1] rounded-full p-2">
-                <Image
-                  priority
-                  src={backArrow}
-                  alt='back icon'
-                  className="w-2 h-2"
-                />
-              </Center>
+        <Box className="max-w-[95rem] mx-auto">
+          <Box className='w-fit'>
+            <Link href={`/dashboard/subjects/${router.query.lessonId}`}>
+              <Flex className="max-w-[97rem] mx-auto space-x-2">
+                <Center className="bg-[#FEEDD1] rounded-full p-2">
+                  <Image
+                    priority
+                    src={backArrow}
+                    alt='back icon'
+                    className="w-2 h-2"
+                  />
+                </Center>
 
-              <Text className="font-bold">Lessons</Text>
-            </Flex>
-          </Link>
+                <Text className="font-bold">Lessons</Text>
+              </Flex>
+            </Link>
+          </Box>
         </Box>
       </Box>
 
       {/* Empty state start */}
       {/* <Box className="w-full px-4 sm:px-6 lg:px-8 mt-10 mb-20">
-        <Box className="bg-[#FEEDD1] py-10 px-10 lg:px-20 mt-14 max-w-4xl rounded-xl mx-auto">
-          <Center>
-            <Box className="text-center">
-              <Box>
-                <Image
-                  alt='icon'
-                  priority
-                  src={preview_subject}
-                  className='w-[20rem] mx-auto'
-                />
-              </Box>
-
-              <Text className="font-semibold text-xl mt-8">
-                This lesson has no Quiz
-              </Text>
-
-              <Flex className="flex-col mt-8 space-y-3 sm:space-y-0 sm:justify-center sm:space-x-3 sm:flex-row">
-                <Link href={`/dashboard/subjects/${router.query.lessonId}`}>
-                  <UnstyledButton
-                    className="px-4 w-52 h-12 text-center font-bold transition duration-75 delay-75 ease-linear hover:bg-[#da9217] rounded-full py-2 bg-[#FAA61A] text-white"
-                  >
-                    Return to lessons
-                  </UnstyledButton>
-                </Link>
-
-                <Link href={`/dashboard/lessons/${router.query.lessonId}/theory`}>
-                  <UnstyledButton
-                    className="px-4 w-52 h-12 text-center font-bold transition duration-75 delay-75 ease-linear hover:bg-[#da9217] rounded-full py-2 bg-[#FAA61A] text-white"
-                  >
-                    Take Theory
-                  </UnstyledButton>
-                </Link>
-              </Flex>
-            </Box>
-          </Center>
-        </Box>
-      </Box>   */}
-      {/* Empty state end */}
-
-      <Box className="w-full px-4 sm:px-6 lg:px-8 mt-4 mb-20">
-        <Flex className="bg-[#FEEDD1] sm:items-center sm:space-x-4 flex-col sm:flex-row py-5 px-8 rounded-xl">
-          <Text className='font-semibold text-2xl truncate'>
-            Assessment
-          </Text>
-
-          <Text className='font-semibold text-[#FAA61A] text-lg truncate'>
-            Introduction to Mathematics
-          </Text>
-        </Flex>
-
-        <Box className="mt-10 max-w-[60rem] mx-auto w-full">
-          <QuizCard 
-            answer={answer}
-            setAnswer={setAnswer}
-            question={currentQuestion}
-            questions={question}
-            currentQuestionIndex={currentQuestionIndex}
-          />
-
-          <Flex className="sm:space-x-3 mt-10 flex-col space-y-3 sm:flex-row sm:space-y-0 sm:justify-end items-end w-full">
-            {currentQuestionIndex > 0 && (
-              <UnstyledButton
-                type="button"
-                onClick={handlePrevious}
-                className="px-10 h-12 text-center w-60 font-bold text-[#777777] transition duration-75 delay-75 ease-linear hover:bg-[#FAA61A] rounded-full py-2 hover:text-white"
-              >
-                Previous Question
-              </UnstyledButton>
-            )}
-
-            {currentQuestionIndex === question.length - 1 ? (
-              <UnstyledButton
-                disabled={!answer && true}
-                type="button"
-                onClick={handleSubmit}
-                className="px-2 w-60 h-12 text-center font-bold disabled:opacity-50 transition duration-75 delay-75 ease-linear hover:bg-[#da9217] rounded-full py-4 bg-[#FAA61A] text-white"
-              >
-                Submit Assessment
-              </UnstyledButton>
-            ) : (
-              <UnstyledButton
-                disabled={!answer && true}
-                type="button"
-                onClick={handleNext}
-                className="px-2 w-60 h-12 text-center font-bold disabled:opacity-50 transition duration-75 delay-75 ease-linear hover:bg-[#da9217] rounded-full py-4 bg-[#FAA61A] text-white"
-              >
-                Next Question
-              </UnstyledButton>
-            )}
-          </Flex>  
-        </Box>
-      </Box>  
-
-      <Modal 
-        opened={opened} 
-        onClose={close}
-        fullScreen
-        withCloseButton={false}
-        padding={0}
-        transitionProps={{ transition: 'fade', duration: 200 }}
-      >
-        <ProfileNav />
-
-        <Box className="w-full px-4 sm:px-6 lg:px-8 mt-10 mb-20">
-          <Flex className="bg-[#FEEDD1] sm:items-center sm:space-x-4 flex-col sm:flex-row py-5 px-8 rounded-xl">
-            <Text className='font-semibold text-2xl truncate'>
-              Assessment
-            </Text>
-
-            <Text className='font-semibold text-[#FAA61A] text-lg truncate'>
-              Introduction to Mathematics
-            </Text>
-          </Flex>
-
-          <Box className="bg-[#FEEDD1] py-10 px-10 lg:px-20 mt-14 max-w-3xl rounded-xl mx-auto">
+        <Box className="max-w-[95rem] mx-auto">
+          <Box className="bg-[#FEEDD1] py-10 px-10 lg:px-20 mt-14 max-w-4xl rounded-xl mx-auto">
             <Center>
               <Box className="text-center">
-                <Text className="font-semibold text-xl">
-                  Assessment score
-                </Text>
+                <Box>
+                  <Image
+                    alt='icon'
+                    priority
+                    src={preview_subject}
+                    className='w-[20rem] mx-auto'
+                  />
+                </Box>
 
-                <Text className="font-semibold text-4xl mt-8">
-                  70%
-                </Text>
-
-                <Text className="text-lg mt-8">
-                  Great job on completing the assessment! Use your score as a stepping stone for improvement. Keep up the good work!
+                <Text className="font-semibold text-xl mt-8">
+                  This lesson has no Quiz
                 </Text>
 
                 <Flex className="flex-col mt-8 space-y-3 sm:space-y-0 sm:justify-center sm:space-x-3 sm:flex-row">
@@ -276,8 +166,124 @@ export default function Quiz (){
               </Box>
             </Center>
           </Box>
-        </Box>  
-      </Modal>
+        </Box>
+      </Box>   */}
+      {/* Empty state end */}
+
+      <Box className="w-full px-4 sm:px-6 lg:px-8 mt-4 mb-20">
+        <Box className="max-w-[95rem] mx-auto ">
+          <Flex className="bg-[#FEEDD1] sm:items-center sm:space-x-4 flex-col sm:flex-row py-5 px-8 rounded-xl">
+            <Text className='font-semibold text-2xl truncate'>
+              Assessment
+            </Text>
+
+            <Text className='font-semibold text-[#FAA61A] text-lg truncate'>
+              Introduction to Mathematics
+            </Text>
+          </Flex>
+
+          <Box className="mt-10 max-w-[60rem] mx-auto w-full">
+            <QuizCard
+              answer={answer}
+              setAnswer={setAnswer}
+              question={currentQuestion}
+              questions={question}
+              currentQuestionIndex={currentQuestionIndex}
+            />
+
+            <Flex className="sm:space-x-3 mt-10 flex-col space-y-3 sm:flex-row sm:space-y-0 sm:justify-end items-end w-full">
+              {currentQuestionIndex > 0 && (
+                <UnstyledButton
+                  type="button"
+                  onClick={handlePrevious}
+                  className="px-10 h-12 text-center w-60 font-bold text-[#777777] transition duration-75 delay-75 ease-linear hover:bg-[#FAA61A] rounded-full py-2 hover:text-white"
+                >
+                  Previous Question
+                </UnstyledButton>
+              )}
+
+              {currentQuestionIndex === question.length - 1 ? (
+                <UnstyledButton
+                  disabled={!answer && true}
+                  type="button"
+                  onClick={handleSubmit}
+                  className="px-2 w-60 h-12 text-center font-bold disabled:opacity-50 transition duration-75 delay-75 ease-linear hover:bg-[#da9217] rounded-full py-4 bg-[#FAA61A] text-white"
+                >
+                  Submit Assessment
+                </UnstyledButton>
+              ) : (
+                <UnstyledButton
+                  disabled={!answer && true}
+                  type="button"
+                  onClick={handleNext}
+                  className="px-2 w-60 h-12 text-center font-bold disabled:opacity-50 transition duration-75 delay-75 ease-linear hover:bg-[#da9217] rounded-full py-4 bg-[#FAA61A] text-white"
+                >
+                  Next Question
+                </UnstyledButton>
+              )}
+            </Flex>
+          </Box>
+        </Box>
+
+        <Modal
+          opened={opened}
+          onClose={close}
+          fullScreen
+          withCloseButton={false}
+          padding={0}
+          transitionProps={{ transition: 'fade', duration: 200 }}
+        >
+          <ProfileNav />
+
+          <Box className="w-full px-4 sm:px-6 lg:px-8 mt-10 mb-20">
+            <Flex className="bg-[#FEEDD1] sm:items-center sm:space-x-4 flex-col sm:flex-row py-5 px-8 rounded-xl">
+              <Text className='font-semibold text-2xl truncate'>
+                Assessment
+              </Text>
+
+              <Text className='font-semibold text-[#FAA61A] text-lg truncate'>
+                Introduction to Mathematics
+              </Text>
+            </Flex>
+
+            <Box className="bg-[#FEEDD1] py-10 px-10 lg:px-20 mt-14 max-w-3xl rounded-xl mx-auto">
+              <Center>
+                <Box className="text-center">
+                  <Text className="font-semibold text-xl">
+                    Assessment score
+                  </Text>
+
+                  <Text className="font-semibold text-4xl mt-8">
+                    70%
+                  </Text>
+
+                  <Text className="text-lg mt-8">
+                    Great job on completing the assessment! Use your score as a stepping stone for improvement. Keep up the good work!
+                  </Text>
+
+                  <Flex className="flex-col mt-8 space-y-3 sm:space-y-0 sm:justify-center sm:space-x-3 sm:flex-row">
+                    <Link href={`/dashboard/subjects/${router.query.lessonId}`}>
+                      <UnstyledButton
+                        className="px-4 w-52 h-12 text-center font-bold transition duration-75 delay-75 ease-linear hover:bg-[#da9217] rounded-full py-2 bg-[#FAA61A] text-white"
+                      >
+                        Return to lessons
+                      </UnstyledButton>
+                    </Link>
+
+                    <Link href={`/dashboard/lessons/${router.query.lessonId}/theory`}>
+                      <UnstyledButton
+                        className="px-4 w-52 h-12 text-center font-bold transition duration-75 delay-75 ease-linear hover:bg-[#da9217] rounded-full py-2 bg-[#FAA61A] text-white"
+                      >
+                        Take Theory
+                      </UnstyledButton>
+                    </Link>
+                  </Flex>
+                </Box>
+              </Center>
+            </Box>
+          </Box>
+        </Modal>
+      </Box>
     </PageLayout>
   )
 }
