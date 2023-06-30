4bb6d69999ea1cd8c2d745c2850b836650d386ed
stephenwayar <stephenbuluswayar@gmail.com>
2023-06-30 17:15:27 +0100

    update: add enrolled students number field to subjects ad card

--- src/components/landing/SubjectAd.tsx
+++ src/components/landing/SubjectAd.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Box, Text } from "@mantine/core";
+import { Box, Text, Flex } from "@mantine/core";
 import Image from "next/image";
+import group_icon from '../../assets/svgs/users_group.svg'
 
 type Props = {
   item: {
     title: string;
     description: string;
-    img: any
+    img: any;
+    students: number
   }
 }
 
 const SubjectAd: React.FC<Props> = ({ item }) => {
   return (
-    <Box className="border-2 rounded-3xl bg-white border-[#E2E2E2] p-4 h-[18.5rem] w-[18rem] flex flex-col">
+    <Box className="border-2 rounded-3xl bg-white border-[#E2E2E2] p-4 h-[18.2rem] w-[18rem] flex flex-col relative">
       <Box className="w-full h-[127px] rounded-xl overflow-hidden">
         <Image
           alt="subject banner"
@@ -26,11 +28,33 @@ const SubjectAd: React.FC<Props> = ({ item }) => {
       </Text>
 
       <Box>
-        <Text className="text-[#888888] truncate text-sm font-semibold mt-1">
+        <Text 
+          style={{
+            display: '-webkit-box',
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: 'vertical',
+            overflow: 'hidden',
+            textOverflow: 'ellipsis',
+          }}
+          className="text-[#888888] text-sm font-semibold mt-1"
+        >
           {item.description}
         </Text>
       </Box>
+
+      <Flex className='items-center justify-end absolute bottom-4 right-4 space-x-2'>
+        <Image
+          alt='icon'
+          src={group_icon}
+          className='w-8 h-8'
+        />
+
+        <Text className="text-[#444444] truncate font-semibold">
+          {item.students}+
+        </Text>
+      </Flex>
     </Box>
+
   );
 };
 
