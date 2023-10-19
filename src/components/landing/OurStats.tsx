fd1028b61f703d3fcd4e7383155fdd20c28278d4
netflams <segope44@gmail.com>
2023-10-19 16:29:57 +0100

    update: update the subject video icon plan

--- src/components/landing/OurStats.tsx
+++ src/components/landing/OurStats.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { BackgroundImage, Box, Text, Flex } from "@mantine/core";
 import Image from "next/image";
 import group_icon from "../../assets/svgs/groups.svg";
+import subject from "../../assets/svgs/subjects.svg";
+import videos_subject from "../../assets/svgs/videos_subject.svg";
+import worksheet from "../../assets/svgs/worksheet.svg";
 import bg_rose from "../../assets/svgs/bg_rose.svg";
+const stats_desc = `With (insert impressive number) sign-ups, we've thrilled countless
+families, making learning a joyful adventure for kids while
+keeping parents actively involved in their child's progress.`;
 
 export default function OurStats() {
   return (
@@ -17,11 +23,7 @@ export default function OurStats() {
               Our Stats
             </Text>
 
-            <Text className="text-center lg:text-left mt-6">
-              With (insert impressive number) sign-ups, we've thrilled countless
-              families, making learning a joyful adventure for kids while
-              keeping parents actively involved in their child's progress.
-            </Text>
+            <Text className="text-center lg:text-left mt-6">{stats_desc}</Text>
           </Box>
 
           <Box className="mt-10 lg:mx-auto">
@@ -36,17 +38,31 @@ export default function OurStats() {
                     />
                   </Box>
 
-                  <Box className="w-[70px]">
+                  {/* <Box className="w-[70px]">
                     <Text className="font-bold text-2xl text-[#00433F] truncate">
                       2k+
                     </Text>
 
                     <Text className="text-sm truncate">Sign ups</Text>
                   </Box>
+
+                  <Box className="w-[20%] h-[39px]">
+                    <Image
+                      alt="icon"
+                      className="w-[39px] h-[39px]"
+                      src={group_icon}
+                    />
+                  </Box> */}
+
+                  <Box className="md:w-[70%]">
+                    <p className="font-bold text-2xl text-[#00433F]">2k+</p>
+
+                    <p className="text-sm md:text-[11px]">Sign ups</p>
+                  </Box>
                 </Flex>
 
                 <Flex className="shadow-md w-48 lg:w-60 hover:scale-150 hover:bg-yellow-100 transition delay-75 hover:cursor-default duration-100 ease-linear sm:!mt-6 h-fit justify-center items-center space-x-5 rounded-lg bg-white p-4">
-                  <Box className="w-[39px] h-[39px]">
+                  {/* <Box className="w-[39px] h-[39px]">
                     <Image
                       alt="icon"
                       className="w-[39px] h-[39px]"
@@ -59,49 +75,61 @@ export default function OurStats() {
                       15+
                     </Text>
 
-                    <Text className="text-sm truncate">
+                    <Text className="text-sm"></Text>
+                  </Box> */}
+
+                  <Box className="w-[20%] h-[39px]">
+                    <Image
+                      alt="icon"
+                      className="w-[39px] h-[39px]"
+                      src={subject}
+                    />
+                  </Box>
+
+                  <Box className="md:w-[70%]">
+                    <p className="font-bold text-2xl text-[#00433F]">15k+</p>
+
+                    <p className="text-sm md:text-[11px]">
                       Subjects with resources
-                    </Text>
+                    </p>
                   </Box>
                 </Flex>
               </Box>
 
               <Box className="space-y-4 sm:space-y-0 sm:ml-6 sm:flex sm:space-x-6">
                 <Flex className="shadow-md w-48 lg:w-60 hover:scale-150 hover:bg-yellow-100 transition delay-75 hover:cursor-default duration-100 ease-linear h-fit justify-center items-center space-x-5 rounded-lg bg-white p-4">
-                  <Box className="w-[39px] h-[39px]">
+                  <Box className="w-[20%] h-[39px]">
                     <Image
                       alt="icon"
                       className="w-[39px] h-[39px]"
-                      src={group_icon}
+                      src={videos_subject}
                     />
                   </Box>
 
-                  <Box className="w-[70px]">
-                    <Text className="font-bold text-2xl text-[#00433F] truncate">
-                      1k+
-                    </Text>
+                  <Box className="md:w-[70%]">
+                    <p className="font-bold text-2xl text-[#00433F]">1k+</p>
 
-                    <Text className="text-sm truncate">
+                    <p className="text-sm md:text-[11px]">
                       Worksheets and activities
-                    </Text>
+                    </p>
                   </Box>
                 </Flex>
 
                 <Flex className="shadow-md w-48 lg:w-60 hover:scale-150 hover:bg-yellow-100 transition delay-75 hover:cursor-default duration-100 ease-linear sm:!mt-6 h-fit justify-center items-center space-x-5 rounded-lg bg-white p-4">
-                  <Box className="w-[39px] h-[39px]">
+                  <Box className="w-[20%] h-[39px]">
                     <Image
                       alt="icon"
                       className="w-[39px] h-[39px]"
-                      src={group_icon}
+                      src={worksheet}
                     />
                   </Box>
 
-                  <Box className="w-[70px]">
-                    <Text className="font-bold text-2xl text-[#00433F] truncate">
-                      500+
-                    </Text>
+                  <Box className="md:w-[70%]">
+                    <p className="font-bold text-2xl text-[#00433F]">500+</p>
 
-                    <Text className="text-sm">Video and Audio Lessons</Text>
+                    <p className="text-sm md:text-[11px]">
+                      Video and Audio Lessons
+                    </p>
                   </Box>
                 </Flex>
               </Box>
